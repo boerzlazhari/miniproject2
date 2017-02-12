e9510a4a60e8c37892cfd7013f3c69622b666b42
Desta Maestrobyte <programmer4.maestrobyte@gmail.com>
2017-02-12 15:10:59 +0700

    kerja praktek

---
+++ application/views/kerja_praktek/pengajuan.php
@@ -0,0 +1,65 @@
+<div class="col-md-12">
+	<div class="box box-primary">
+		<div class="box-header with-border">
+		    <h3 class="box-title">Daftar Pengajuan Kerja Praktek</h3>
+		    <div class="box-tools pull-right">
+		      	<a href="pengajuan/tambah" class="btn btn-default btn-sm">
+		      		<i class="fa fa-plus"></i>
+		      		Tambah Pengajuan
+		      	</a>
+		    </div>	  
+		</div>
+		<div class="box-body">
+			<table class="table table-striped table-bordered table-hover table-condensed" id="table_pengajuan">
+				<thead>
+					<tr>
+						<th width="4%" class="text-center">#</th>
+						<th class="text-center">Nama Perusahaan</th>
+						<th width="50%" class="text-center">Alamat</th>
+						<th width="10%" class="text-center">Status</th>
+						<th width="10%" class="text-center">Aksi</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class="text-center">1</td>
+						<td>PT. PLN Indonesia</td>
+						<td>Jl. Asia Afrika no.9</td>
+						<td class="text-center">
+							<span class="badge bg-red">Ditolak</span>	
+						</td>
+						<td class="text-center">
+							<a href="" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></a>
+						</td>
+					</tr>
+					<tr>
+						<td class="text-center">2</td>
+						<td>PT. Astra Bersama Indonesia</td>
+						<td>Jl. Soekarno Hatta no.22</td>
+						<td class="text-center">
+							<span class="badge bg-grey">Menunggu Konfirmasi</span>
+						</td>
+						<td class="text-center">
+							<a href="" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></a>
+						</td>
+					</tr>
+					<tr>
+						<td class="text-center">3</td>
+						<td>PT. LEN</td>
+						<td>Jl. Soekarno Hatta no.229</td>
+						<td class="text-center">
+							<span class="badge bg-blue">Disetujui</span>
+						</td>
+						<td class="text-center">
+							<a href="" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></a>
+						</td>
+					</tr>
+
+				</tbody>
+			</table>
+		</div>
+		<!-- <div class="box-footer">
+			The footer of the box
+		</div> -->
+	</div>
+</div>
